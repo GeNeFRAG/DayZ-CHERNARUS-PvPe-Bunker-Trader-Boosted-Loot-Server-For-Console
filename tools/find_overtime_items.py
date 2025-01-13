dd2b819ce847c705b5e7649d6df805d09316c0ee
GeNeFRAG <froehlich.gerhard@gmail.com>
2025-01-13 15:43:31 +0100

    New json for blake lake pub, loot optimization and other stuff

--- tools/find_overtime_items.py
+++ tools/find_overtime_items.py
@@ -12,13 +12,22 @@ with open(log_file_path, 'r') as file:
     log_data = file.readlines()
 
 overtime_items = set()
+performance_drop_items = set()
 
 for line in log_data:
-    match = re.search(r'Item \[\d+\] causing search overtime: "(.*?)"', line)
-    if match:
-        overtime_items.add(match.group(1))
+    overtime_match = re.search(r'Item \[\d+\] causing search overtime: "(.*?)"', line)
+    performance_match = re.search(r'LootRespawner\] \(PRIDummy\) :: Item \[\d+\] is hard to place, performance drops: "(.*?)"', line)
+    if overtime_match:
+        overtime_items.add(overtime_match.group(1))
+    if performance_match:
+        performance_drop_items.add(performance_match.group(1))
 
 print(f"Number of unique items causing search overtime: {len(overtime_items)}")
 print("Unique items causing search overtime:")
 for item in overtime_items:
+    print(item)
+
+print(f"\nNumber of unique items causing performance drops: {len(performance_drop_items)}")
+print("Unique items causing performance drops:")
+for item in performance_drop_items:
     print(item)
