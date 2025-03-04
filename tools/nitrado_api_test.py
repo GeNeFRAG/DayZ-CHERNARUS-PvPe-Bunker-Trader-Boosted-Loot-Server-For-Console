6859614c5106eb202e19c95c9330d41f801bedf9
GeNeFRAG <froehlich.gerhard@gmail.com>
2025-03-04 12:01:25 +0100

    remove secrets

--- tools/nitrado_api_test.py
+++ tools/nitrado_api_test.py
@@ -7,11 +7,64 @@ from urllib3.exceptions import InsecureRequestWarning
 # Disable SSL warnings
 urllib3.disable_warnings(InsecureRequestWarning)
 
-# Hard-coded values for testing
-TOKEN = "M8qp_kcGLejWdkFG4vx9cu8fD02VRiEweegScYjBs9tU5emUS6DV6L9msaucEV0sC2RjZQVZr8ORvtUWuEO7_1VGq3SCFM3wGU-I"
-SERVER_ID = "ni8292907_1"
-NITRADO_ID = "16355842"
 
+def get_file_stats(self, remote_dirs):
+    """
+    Get file information from the server.
+
+    Args:
+        remote_dirs (list): List of remote directories to get file stats from.
+
+    Returns:
+        list: List of file stats, or None if an error occurred.
+    """
+    print(f"Get file information from Nitrado...")
+    base_path = f"/games/{self.server_id}/ftproot/dayzxb_missions/dayzOffline.chernarusplus/"
+
+    try:
+        # Get file stats from the main directory
+        url = f'{NITRADO_API_BASE_URL}{self.nitrado_id}{self.remote_base_path}/list?dir={base_path}'
+        response = requests.get(url, headers=self.headers, verify=self.ssl_verify)
+        
+        if response.status_code == 200:
+            response_json = response.json()
+            file_stats = [
+                {
+                    'path': file['path'],
+                    'modified_at': datetime.fromtimestamp(file['modified_at']).isoformat(),
+                    'name': file['name']
+                }
+                for file in response_json['data']['entries'] if file['type'] == 'file'
+            ]
+        else:
+            print(f"Error fetching file stats: {response.text}")
+            return None
+        
+        # Get file stats from each remote directory
+        for remote_dir in remote_dirs:
+            dir_url = f'{NITRADO_API_BASE_URL}{self.nitrado_id}{self.remote_base_path}/list?dir={base_path}/{remote_dir}'
+            dir_response = requests.get(dir_url, headers=self.headers, verify=self.ssl_verify)
+            if dir_response.status_code == 200:
+                dir_response_json = dir_response.json()
+                file_stats.extend([
+                    {
+                        'path': file['path'],
+                        'modified_at': datetime.fromtimestamp(file['modified_at']).isoformat(),
+                        'name': file['name']
+                    }
+                    for file in dir_response_json['data']['entries'] if file['type'] == 'file'
+                ])
+            elif dir_response.status_code == 404:
+                print(f"ERROR: Directory not found: {dir_response.status_code} {remote_dir}")
+                return None
+            else:
+                print(f"Error fetching file stats from {remote_dir}: {dir_response.status_code}: {dir_response.text}")
+                return None
+        print(f"Successfully fetched {len(file_stats)} file stats fromo Nitrado")
+        return file_stats
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+    return None
 
 def debug_nitrado_download():
     # Base headers
